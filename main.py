b90d39aa73cb98c04f6d33802748cf4e74b48a7e
Marco García <103073200+marcgarmen@users.noreply.github.com>
2025-06-10 14:21:39 -0600

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional, Any
 import pandas as pd
 from prophet import Prophet
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class UsageHistory(BaseModel):
     ingredient: Optional[str] = None
